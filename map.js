de245c500bf37db65133061fe6ef983a16765a12
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-11-08 16:41:15 +0700

    Create map.js

---
+++ map.js
@@ -0,0 +1,15 @@
+if blockman-go.v1.0
+if true 
+if true [{"blockman go"}]
+
+Advance.js
+Advance2.js
+Not.responding.py
+
+//pemilihan peta & profile
+Selection.map[{bad.wars}]~profile[{"blockman-go.py"}]
+apikey:blockman
+password:blockman
+ussername:blockman
+viewers:1,863,364,624,228,991
+youtube:blockman-go
