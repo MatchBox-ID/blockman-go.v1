2281b0935717b9e8751dfadee38eab5cbf746d74
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-11-08 16:36:43 +0700

    Create blockman.py

---
+++ blockman.py
@@ -0,0 +1,21 @@
+If python
+If selection
+
+{
+  "tanah.meter":'7518 Meter²'
+  "ketebalan.meter":'5 block'
+  "guardian":'noob'
+}
+{ 
+  "shirt":'noob','noob.hallowen'
+  "shirt":'superman','superman.hallowen'
+}
+{
+  "profile":account.matchbox
+  "id.User":076518653
+  "password":admingm1
+}
+{
+  "map":bad.wars
+  "map":bad.wars.acara.2
+}
