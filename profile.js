1954d030fdc4f3df85446fb1f570caec7694840d
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-11-08 16:51:30 +0700

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,19 @@
+}
+  "ussername":'gloryadevelopment' //ganti dengan ussername lu di blockman, harus asli kalau gak gabakal ada history nya dan bahkan pasti error
+  "id":'755185629' //ganti dengan id blockman go lu
+  "cheat-ban":[{'coin', jumlah:10.000}] //buat cheat blockman go lu, dijamin aman soalnya yang kedeteksi aplikasi adalah history topup bukan cheat
+  "cheat-ban2":[{'topup':${cheat-ban}, jumlah.topup:10.000}] //samain jumlah nya dengan yang lu cheat biar ga kebanned aplikasi tolol
+  "topup":[{'itemku':${cheat-ban}, gloryhistory.topuptodate}] // gausah diganti 
+{
+}
+  "semi~topup.history, Weeklyupdate, Monthlyupdate,"
+  "semi~topup.history, Dailyupdate, Yearsupdate"
+  "Weeklyupdates:is.true
+  "Monthlyupdates:is.true
+  "Yearsupdate:is.false // masih progress jangan di ganti true dulu
+{
+Blockmango
+Blockmango.Update
+Blockmango.Session
+Blockmango.Cheat&history
+if true blockmango.id
